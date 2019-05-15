4808f6ffb10b5f581f832554fb2b44bf85372c18
Buck Doyle <b@chromatin.ca>
2019-05-14 21:26:58 -0500

    Remove unused property

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,7 +4,6 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
-  @tracked camera;
   @tracked error;
   @tracked barcode;
 }
