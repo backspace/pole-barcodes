6fa6d9815adbb72dee4bd0153bd2dd9978e38e81
Buck Doyle <b@chromatin.ca>
2019-05-17 23:45:27 -0500

    Move scanner to its own route
    This is a temporary place for testing until I can
    incorporate the reader as part of the gathering
    interface.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,4 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
-  @tracked error;
-  @tracked barcode;
 }
