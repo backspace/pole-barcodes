a032f23246ea5d98b78e41c70f2057fe330cd195
Buck Doyle <b@chromatin.ca>
2019-05-20 00:57:58 -0500

    Add component to load/display attachment image
    Load/display? 🤔
    
    But a huge thank you to @mattmarcum whose technique I shoved
    in here, probably belongs elsewhere, but:
    https://github.com/pouchdb-community/ember-pouch/issues/115#issuecomment-189577462

---
+++ app/components/image-attachment/component.js
@@ -0,0 +1,24 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+import { task } from 'ember-concurrency';
+import blobUtil from 'blob-util';
+
+export default class ImageAttachment extends Component {
+  @service store;
+
+  constructor() {
+    super(...arguments);
+  }
+
+  @task(function*() {
+    let model = this.args.model;
+
+    let adapter = this.store.adapterFor(model.constructor.modelName);
+
+    let attachment = this.args.attachment;
+    let data = yield adapter.db.rel.getAttachment(adapter.getRecordTypeName(model.constructor), model.id, attachment.name);
+
+    return yield blobUtil.blobToBase64String(data);
+  }) base64StringTask;
+}
