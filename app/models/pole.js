aabefdef3c8e8c483beeccece21202a7a5d56b4a
Buck Doyle <b@chromatin.ca>
2019-05-20 14:47:27 -0500

    Change map to centre on bounds of poles

--- app/models/pole.js
+++ app/models/pole.js
@@ -1,10 +1,20 @@
+/* global L */
 import DS from 'ember-data';
 const { Model, attr } = DS;
+import { computed } from '@ember/object';
 
 export default class PoleModel extends Model {
   @attr('string') barcode;
   @attr('number') latitude;
   @attr('number') longitude;
+
+  @computed('latitude', 'longitude', function() {
+    if (this.latitude && this.longitude) {
+      return L.latLng(this.latitude, this.longitude);
+    } else {
+      return null;
+    }
+  }) position;
 
   @attr('attachments', { defaultValue: () => [] } ) photos;
 
