7a0b3af993cabad6bd48343431a6d615ff33003d
Buck Doyle <b@chromatin.ca>
2019-05-22 19:44:54 -0500

    Move pole listing to show on pole index only

--- app/controllers/poles/index.js
+++ app/controllers/poles/index.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import { filter, mapBy } from '@ember/object/computed';
 
-export default class PolesController extends Controller {
+export default class PolesIndexController extends Controller {
   @mapBy('model', 'position') allPositions;
   @filter('allPositions', position => position) positions;
 }
